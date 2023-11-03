365877d4b76c880e52c0906d104ad6fc57a0c31c
Kyle Simmons <simmonskyleb@gmail.com>
2023-11-03 01:56:25 -0500

    improve errors in gl-utils.js

--- js/gl-utils.js
+++ js/gl-utils.js
@@ -7,7 +7,7 @@ function makeProgram(gl, vertexShaderSource, fragmentShaderSource) {
 
         if (!gl.getShaderParameter(shader, gl.COMPILE_STATUS)) {
             const info = gl.getShaderInfoLog(shader);
-            throw new Error(`Could not compile shader from ${shaderSource}. \n\n${info}`);
+            throw new Error(`Could not compile shader: ${shaderSource} \n\n${info}`);
         }
 
         return shader;
@@ -32,33 +32,29 @@ function makeBuffer(gl, data, bufferType) {
 
 // Load vertex and index data from an .obj file
 async function loadOBJ(path) {
-    try {
-        const response = await fetch(path);
-        if (!response.ok) {
-            throw new Error(`Failed to fetch .obj file: ${response.status} ${response.statusText}`);
-        }
-
-        const objData = await response.text();
-        const vertices = [];
-        const indices = [];
-        const lines = objData.split('\n');
-        for (const line of lines) {
-            const parts = line.trim().split(' ');
-            if (parts[0] === 'v') {
-                const x = parseFloat(parts[1]);
-                const y = parseFloat(parts[2]);
-                const z = parseFloat(parts[3]);
-                vertices.push(x, y, z);
-            } else if (parts[0] === 'f') {
-                const a = parseInt(parts[1]) - 1; // Subtract 1 to convert to 0-based index
-                const b = parseInt(parts[2]) - 1;
-                const c = parseInt(parts[3]) - 1;
-                indices.push(a, b, c);
-            }
-        }
-
-        return { vertices: new Float32Array(vertices), indices: new Uint16Array(indices) };
-    } catch (error) {
-        throw error;
+    const response = await fetch(path);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch .obj file from ${path}\nResponse: ${response.status} ${response.statusText}`);
     }
+
+    const objData = await response.text();
+    const vertices = [];
+    const indices = [];
+    const lines = objData.split('\n');
+    for (const line of lines) {
+        const parts = line.trim().split(' ');
+        if (parts[0] === 'v') {
+            const x = parseFloat(parts[1]);
+            const y = parseFloat(parts[2]);
+            const z = parseFloat(parts[3]);
+            vertices.push(x, y, z);
+        } else if (parts[0] === 'f') {
+            const a = parseInt(parts[1]) - 1; // Subtract 1 to convert to 0-based index
+            const b = parseInt(parts[2]) - 1;
+            const c = parseInt(parts[3]) - 1;
+            indices.push(a, b, c);
+        }
+    }
+
+    return { vertices: new Float32Array(vertices), indices: new Uint16Array(indices) };
 };
